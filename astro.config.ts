94212dd86e7b3d580a5b3d671bb4773c859d8f2a
meduvers <meduvers@gmail.com>
2024-08-07 15:38:56 +0000

    cvv

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"
 
 // Astro integration imports
-import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import compress from "astro-compress"
 import { VitePWA } from "vite-plugin-pwa"
@@ -12,12 +11,6 @@ import { manifest, seoConfig } from "./utils/seoConfig"
 export default defineConfig({
 	site: seoConfig.baseURL,
 	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-				path: "./tailwind.config.js"
-			}
-		}),
 		sitemap(),
 		compress()
 	],

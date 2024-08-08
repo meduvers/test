a6c162e4eae599032d49fe438df8b593c0bd5e96
meduvers <meduvers@gmail.com>
2024-08-08 07:48:40 +0000

    mm

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,12 +5,12 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://eduvers-test.netlify.app//", // Change this to your production URL.
 	description:
 		"eduvers is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
 	type: "website",
 	image: {
-		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
+		url: "https://eduvers-test.netlify.app/favicons/favicon.svg", // Change this to your website's thumbnail.
 		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
 		width: 1200,
 		height: 630
